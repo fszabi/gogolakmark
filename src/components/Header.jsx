c84169147e5a37ec1c67d662b16c7b953441770e
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-01-12 13:42:43 +0100

    logo changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ export default function Header() {
     <header className="bg-backgroundColor py-3 border-b border-gray-300">
       <div className="wrapper 2xl:wrapper-big 3xl:wrapper-bigger flex gap-10 items-center">
         <picture>
-          <source srcSet="https://picsum.photos/64" />
+          <source srcSet="assets/logo.png" />
           <img
-            className="rounded-xl"
-            src="https://picsum.photos/64"
+            className="rounded-xl w-20 h-20"
+            src="assets/logo.png"
             alt="logo"
           />
         </picture>
